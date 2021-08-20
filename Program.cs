c377b4d7c8a549b76139c3f973989cd002a85dc2
Lucas de Souza Santos <lucas210souza@gmail.com>
2021-08-20 01:48:44 -0300

    subindo arquivos de testes

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DecolaTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 34;
+            int b = 90;
+            int soma = a + b;
+
+            Console.WriteLine($"Soma {a} + {b} = {soma}");
+
+            if(soma >= 70)
+            {
+                Console.WriteLine("Média Boa");
+            }
+            else
+            {
+                Console.WriteLine("Pode Melhora");
+            }
+        }
+    }
+}
