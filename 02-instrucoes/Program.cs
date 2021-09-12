a454b99f934f45131d05d02c0c5db5e3b85334c4
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2021-09-11 22:40:08 -0300

    subindo

---
+++ 02-instrucoes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02_instrucoes
+{
+    class Program
+    {
+        static void Declaracoes()
+        {
+            int a;
+            int b =2, c = 3;
+            const int d = 4;
+            a = 1;
+            Console.WriteLine(a + b + c + d);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
